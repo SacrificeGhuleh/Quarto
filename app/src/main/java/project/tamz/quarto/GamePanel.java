0bbb00b3bffd2cd39ef3ef722f938c23bb7e2b5c
SacrificeGhuleh <zvonek.r@seznam.cz>
2017-12-02 21:24:05 +0100

    Main activity     Spousti GamePanel
    GamePanel
        Spousti GameThread
    
    GameThread
        Zarucuje pri behu maximalni pocet snimku za vterinu

---
+++ app/src/main/java/project/tamz/quarto/GamePanel.java
@@ -0,0 +1,97 @@
+package project.tamz.quarto;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by Richard Zvonek on 02/12/2017.
+ */
+
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
+
+
+    private GameThread thread;
+
+    public GamePanel(Context ctx) {
+        super(ctx);
+
+        getHolder().addCallback(this);
+
+        this.thread = new GameThread(getHolder(), this);
+
+        setFocusable(true);
+
+    }
+
+    /**
+     * This is called immediately after the surface is first created.
+     * Implementations of this should start up whatever rendering code
+     * they desire.  Note that only one thread can ever draw into
+     * a {@link Surface}, so you should not draw into the Surface here
+     * if your normal rendering will be in another thread.
+     *
+     * @param holder The SurfaceHolder whose surface is being created.
+     */
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        this.thread = new GameThread(getHolder(), this);
+        thread.setRunning(true);
+        thread.start();
+    }
+
+    /**
+     * This is called immediately after any structural changes (format or
+     * size) have been made to the surface.  You should at this point update
+     * the imagery in the surface.  This method is always called at least
+     * once, after {@link #surfaceCreated}.
+     *
+     * @param holder The SurfaceHolder whose surface has changed.
+     * @param format The new PixelFormat of the surface.
+     * @param width  The new width of the surface.
+     * @param height The new height of the surface.
+     */
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    /**
+     * This is called immediately before a surface is being destroyed. After
+     * returning from this call, you should no longer try to access this
+     * surface.  If you have a rendering thread that directly accesses
+     * the surface, you must ensure that thread is no longer touching the
+     * Surface before returning from this function.
+     *
+     * @param holder The SurfaceHolder whose surface is being destroyed.
+     */
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        while (true) {
+            try {
+                thread.setRunning(false);
+                thread.join();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            retry = false;
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+    }
+}
---
+++ app/src/main/java/project/tamz/quarto/GameThread.java
@@ -0,0 +1,79 @@
+package project.tamz.quarto;
+
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+
+/**
+ * Created by Richard Zvonek on 02/12/2017.
+ */
+
+public class GameThread extends Thread {
+    public static final int FPS_LOCK = 30;
+    public static Canvas canvas;
+    public double avgFPS;
+    private SurfaceHolder surfaceHolder;
+    private GamePanel gamePanel;
+    private boolean running;
+
+    public GameThread(SurfaceHolder surfaceHolder, GamePanel gamePanel) {
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.gamePanel = gamePanel;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    @Override
+    public void run() {
+        long startTime;
+        long timeMillis = 1000 / FPS_LOCK;
+        long waitTime = 0;
+        int frameCunt = 0;
+        long totalTime = 0;
+        long targetTime = 1000 / FPS_LOCK;
+
+        while (this.running) {
+            startTime = System.nanoTime();
+            canvas = null;
+            try {
+                canvas = this.surfaceHolder.lockCanvas();
+                synchronized (surfaceHolder) {
+                    this.gamePanel.update();
+                    this.gamePanel.draw(canvas);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (canvas != null) {
+                    try {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+
+            timeMillis = (System.nanoTime() - startTime) / 1000000;
+            waitTime = targetTime - timeMillis;
+            try {
+                if (waitTime > 0) {
+                    sleep(waitTime);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            totalTime += System.nanoTime() - startTime;
+            frameCunt++;
+            if (frameCunt == FPS_LOCK) {
+                avgFPS = 1000 / ((totalTime / frameCunt) / 1000000);
+                frameCunt = 0;
+                totalTime = 0;
+                System.out.println("FPS: " + avgFPS);
+            }
+        }
+    }
+}
